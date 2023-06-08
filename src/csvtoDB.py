97cdf9de9847b7ca17543b8df37566862f2e0e1d
Ujit Kumar <ujitkumar1@gmail.com>
2023-06-09 00:11:48 +0530

    able to place the orders'

---
+++ src/csvtoDB.py
@@ -0,0 +1,25 @@
+import pandas as pd
+from sqlalchemy import create_engine
+
+# CSV file path
+csv_file = '../database/foodItem.csv'
+# SQLite database file path
+db_file = '../database/database.sqlite3'
+
+# Table name in the SQLite database
+table_name = 'menu'
+
+# Load the CSV data into a Pandas DataFrame
+df = pd.read_csv(csv_file)
+
+# Create the database connection URL
+db_url = f'sqlite:///{db_file}'
+
+# Create an SQLAlchemy engine
+engine = create_engine(db_url)
+
+# Write the DataFrame to the SQLite database table
+df.to_sql(table_name, engine, index=False, if_exists='replace')
+
+# Close the database connection
+engine.dispose()
--- src/main.py
+++ src/main.py
@@ -1,6 +1,9 @@
 from flask import Flask, make_response, render_template,request,session,redirect
 from flask_sqlalchemy import SQLAlchemy
 import os
+from flask_login import login_required
+from datetime import datetime
+
 
 app = Flask(__name__)
 app.secret_key = "IITMBS21"
@@ -16,6 +19,20 @@ class User(db.Model):
     username = db.Column(db.String(20), unique=True, nullable=False)
     password = db.Column(db.String(60), nullable=False)
 
+class Menu(db.Model):
+    __tablename__ = 'menu'
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(70),nullable=False)
+    price = db.Column(db.Integer,nullable=False)
+
+class Orders(db.Model):
+    __tablename__= 'order'
+    id = db.Column(db.Integer, primary_key=True)
+    item = db.Column(db.String(70),nullable=False)
+    qty = db.Column(db.Integer, nullable=False)
+    price = db.Column(db.Integer, nullable=False)
+    date = db.Column(db.DateTime, default=datetime.utcnow)
+
 @app.route("/")
 def main():
     return make_response(render_template('index.html'), 200)
@@ -26,17 +43,58 @@ def login():
         return make_response(render_template('login.html'), 200)
 
     elif request.method =="POST":
+        print("ok")
         username = request.form['username']
         password = request.form['password']
         user = User.query.filter_by(username=username).first()
+        print(user)
         if user and user.password == password:
             session['username'] = username
             session['user_id'] = user.id
+            print("ok")
             return redirect('/home')# change this
         else:
             return 'Invalid username/password combination', 400
 
+@login_required
+@app.route("/home")
+def home():
+    food = fetchMenu()
+    return make_response(render_template('home.html',menu = food), 200)
 
+@login_required
+@app.route("/place-order",methods =["GET","POST"])
+def placeOrder():
+    if request.method == "POST":
+        orders = fetchOrderData(request.form)
+        # print()
+        for oneOrder in orders:
+            placeorder = Orders(item=oneOrder[0],qty=oneOrder[2],price=(oneOrder[2]*oneOrder[1]))
+            db.session.add(placeorder)
+            db.session.commit()
+        return str(orders)
+
+def fetchOrderData(form):
+    foodMenu = fetchMenu()
+    orderData = []
+    # print(form)
+    for item in form:
+        if form[item] != "":
+            index = int(item.split("_")[1])
+            qty = int(form[item])
+            orderItem = foodMenu[index].split(' - ')
+            item = orderItem[0]
+            price = float(orderItem[1].split(" ")[1])
+            data = [item, price, qty]
+            orderData.append(data)
+    return orderData
+
+def fetchMenu():
+    menu = Menu.query.all()
+    menuDict = {}
+    for i in menu:
+        menuDict[i.id] = i.name+" - Rs. "+str(i.price)
+    return (menuDict)
 
 if __name__=="__main__":
     with app.app_context():
